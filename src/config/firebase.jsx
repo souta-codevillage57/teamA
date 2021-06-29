8012ac079f2dd9387a7a954d0f2634d0342cc6b8
souta-codevillage57 <souta1846@gmail.com>
2021-06-29 20:06:16 +0900

    firebaseを導入

---
+++ src/config/firebase.jsx
@@ -0,0 +1,14 @@
+import * as firebase from 'firebase/app'
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBA699H8Np4nH1c0kQhGCat-d3lopD58_c",
+    authDomain: "teama-f9684.firebaseapp.com",
+    projectId: "teama-f9684",
+    storageBucket: "teama-f9684.appspot.com",
+    messagingSenderId: "142954621586",
+    appId: "1:142954621586:web:04eaa699b1b8e757a1d44b"
+}
+
+firebase.initializeApp(firebaseConfig)
+
+export default firebase
