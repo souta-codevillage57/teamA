12e660ba64fb85d4d24673126cbf26bfdb8aef03
souta-codevillage57 <souta1846@gmail.com>
2021-07-04 18:09:15 +0900

    push

---
+++ src/pages/answers/Question.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Question = (props) => {
+
+    return (
+        <h2 className="question">{props.question}</h2>
+    );
+}
+
+export default Question;
---
+++ src/pages/answers/TagList.jsx
@@ -0,0 +1,22 @@
+
+import React from 'react';
+import Tag from './Tag'
+
+
+const TagList = (props) => {
+
+    return (
+        <>
+            <Tag tag={props.tag} />
+            <Tag tag={props.tag} />
+            <Tag tag={props.tag} />
+            <Tag tag={props.tag} />
+            <Tag tag={props.tag} />
+            <Tag tag={props.tag} />
+            <Tag tag={props.tag} />
+        </>
+    );
+}
+
+
+export default TagList;
